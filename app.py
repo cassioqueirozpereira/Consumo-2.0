3d164cafd5cec09d898ca4a1e1eaffcd3450080e
Cassio <cassioqueirozpereira@gmail.com>
2025-10-13 22:44:03 -0300

    Add new collors of files differents

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ COR_MAP_PT_BR = { # Dicionário para traduzir os nomes das cores do Inglês para
     "Blue": "Azul",
     "Yellow": "Amarelo",
     "Luster": "Brilho",
-    "Reactive": "Reativo"
+    "Reactive": "Reativo",
     "ESMERIL": "Luster"
 }
 
